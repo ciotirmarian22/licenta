1f1c210f34f1f440dc4d2d5144d9f3759d37db77
ciotirmarian22 <ciotirmarian80@gmail.com>
2023-08-25 21:07:28 +0300

    button enabled by product and number selected

--- public/js/calorii.js
+++ public/js/calorii.js
@@ -1,27 +1,31 @@
 // calorii.js
 
 document.addEventListener("DOMContentLoaded", function () {
-    const addTextButton = document.getElementById("addTextButton");
+    const addToDatabaseButton = document.getElementById("addToDatabaseButton");
+    const eventTextInput = document.getElementById("eventText"); // Numeric input element
+    const selectedProductNameElement = document.getElementById("selectedProductName");
 
-    if (addTextButton) {
-        addTextButton.addEventListener("click", function () {
-            const eventText = document.getElementById("eventText").value;
-            console.log("Event Text:", eventText);
+    if (addToDatabaseButton && eventTextInput && selectedProductNameElement) {
+        // Add input event listener to the numeric input
+        eventTextInput.addEventListener("input", function () {
+            const eventText = eventTextInput.value;
+            const isNumberGreaterThanZero = parseInt(eventText) > 0;
 
-            // Send the eventText value to the server
-            fetch('/add-event-text', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ eventText })
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log("Server response:", data);
-            })
-            .catch(error => {
-                console.error("Error:", error);
+            // Enable/disable the button based on both conditions
+            addToDatabaseButton.disabled = !(isNumberGreaterThanZero && selectedProductNameElement.textContent !== "");
+
+            console.log("isNumberGreaterThanZero:", isNumberGreaterThanZero);
+        });
+
+        // Add click event listener to the dynamic search results
+        const searchResults = document.querySelectorAll(".search-result-item");
+        searchResults.forEach(result => {
+            result.addEventListener("click", function () {
+                const selectedProductName = result.textContent;
+
+                // Enable/disable the button based on both conditions
+                addToDatabaseButton.disabled = !(parseInt(eventTextInput.value) > 0 && selectedProductName !== "");
+                selectedProductNameElement.textContent = selectedProductName; // Update selected product name display
             });
         });
     }
